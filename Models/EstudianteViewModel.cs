715fdb9ccd38e0ee91d87cfd02247da0a1c35035
Omar Genao <omargenao78@gmail.com>
2025-06-05 22:09:11 -0400

    comit modelo de datos

--- Models/EstudianteViewModel.cs
+++ Models/EstudianteViewModel.cs
@@ -1,6 +1,48 @@
-﻿namespace Actividad3LengProg3.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Actividad3LengProg3.Models
 {
     public class EstudianteViewModel
     {
+        [Required]
+        [StringLength(100)]
+        public required string NombreCompleto { get; set; }
+
+        [Required]
+        [StringLength(15, MinimumLength = 6)]
+        public required string Matricula { get; set; }
+
+        [Required]
+        public required string Carrera { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public required string CorreoInstitucional { get; set; }
+
+        [Phone]
+        [MinLength(10)]
+        public required string Telefono { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime FechaNacimiento { get; set; }
+
+        [Required]
+        public required string Genero { get; set; }
+
+        [Required]
+        public required string Turno { get; set; }
+
+        [Required]
+        public required string TipoIngreso { get; set; }
+
+        public bool EstaBecado { get; set; }
+
+        [Range(0, 100)]
+        public int? PorcentajeBeca { get; set; }
+
+        [Range(typeof(bool), "true", "true", ErrorMessage = "Debes aceptar los términos.")]
+        public bool AceptaTerminos { get; set; }
+   
     }
 }
